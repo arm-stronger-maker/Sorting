9282c205680911138e4b4183427ceafe1c89a5d6
Raman-2003 <ramankishore838@gmail.com>
2024-08-07 06:59:26 +0530

    better hash function using Prime

--- Practice/normalHash.js
+++ Practice/normalHash.js
@@ -1,5 +1,5 @@
 // Understand collision
-
+// InEffcient hash class
 class Hash{
     constructor(size){
         this.size = size;
@@ -43,11 +43,45 @@ const hashTest = new Hash(50)
 
 
 hashTest.insertValue('name', 'Anu')
-hashTest.print()
 
-// hashTest.insertValue('name', 'Tamil')
-// hashTest.print()
+hashTest.insertValue('name', 'Tamil')
 
-console.log(hashTest.getValue('name'))
+console.log(hashTest.getValue('name')) // collision occurs.
 
-hashTest.asciiGetting(17)
+
+
+class hashPractice{
+    constructor(size){
+        this.size = size;
+        this.items = new Array(size);
+    }
+
+    hashIndex(key){
+        let total = 0;
+        for(let i=0; i<key.length; i++){
+            total += (total * 31 + key.charCodeAt(i))%this.size
+        }
+        return total;
+    }
+
+    set(key, value){
+        let index = this.hashIndex(key);
+        this.items[index] = value;
+    }
+
+    print(){
+        for(let i=0; i<this.items.length; i++){
+            if(this.items[i]){
+                console.log(i, this.items[i]);
+                
+            }
+        }
+    }
+}
+
+
+
+const testingHash = new hashPractice(50)
+
+testingHash.set('age', 20)
+testingHash.print()
