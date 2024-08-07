c492c62ef868405588772e1c7c15db9bfeaef7d5
Raman-2003 <ramankishore838@gmail.com>
2024-08-07 07:48:13 +0530

    Perfect linear probing

--- Practice/normalHash.js
+++ Practice/normalHash.js
@@ -85,3 +85,66 @@ const testingHash = new hashPractice(50)
 
 testingHash.set('age', 20)
 testingHash.print()
+testingHash.set('age', 22)
+testingHash.print()
+// But the issue is, it overwrites the pre exisiting valu, means also it have the collision issue. It is only applicable for better distribution. In case of same keys are occurs, it overwrites.
+
+
+// there are some techniques available to prevent collisons  such as Linear probing, seperate chaining.
+
+class hashwithOutCollision{
+    constructor(size){
+        this.items = new Array(size).fill(null); //  should be initialized with null values to begin with.
+        this.size = size;  
+    }
+
+    betterHash(key){
+        let total = 0;
+        for(let i=0; i<key.length; i++){
+            total = (total * 31 + key.charCodeAt(i)) % this.size  // ****
+        }
+        return total;
+    }
+    
+    /*  Without has the size monitor 
+    insertValue(key){
+        let index = this.betterHash(key);
+        while(this.items[index]!==null){
+            index = (index+1)%this.size;
+        }
+        this.items[index] = key;
+    }
+    */
+
+    // With size monitor.
+    insertValueWith(key){
+        let index = this.betterHash(key);
+        let startIndex = index; //Store the start index to detect if the table is full
+        while(this.items[index] !== null){
+            index = (index+1)%this.size;
+            if(index === startIndex){
+                console.log('HASH is Full');
+                return;
+            }
+        }
+        this.items[index] = key;
+    }
+}
+
+const withOutCollision = new hashwithOutCollision(7);
+
+withOutCollision.insertValueWith('Raman')
+withOutCollision.insertValueWith('Kishore')
+withOutCollision.insertValueWith('Armstrong')
+withOutCollision.insertValueWith('Leader')
+withOutCollision.insertValueWith('Typing')
+withOutCollision.insertValueWith('Food')
+withOutCollision.insertValueWith('Boat')
+withOutCollision.insertValueWith('Where the dresses')
+// withOutCollision.insertValueWith('Move accross')
+// withOutCollision.insertValue('Walk') ...It leads to infinte loop. Because array attains its max size.
+// Also, Linear probing mostly follows like circular queue. Slot is occupied one by one. Sequentially. 
+// Rewrites the insertFUNtion.
+
+console.log(withOutCollision.items);
+
